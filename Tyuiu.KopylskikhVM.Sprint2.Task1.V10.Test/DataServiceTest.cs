55ac76b3a395b1d2b5a064cb898e2b9623d94c47
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-29 03:10:53 +0500

    Task1

---
+++ Tyuiu.KopylskikhVM.Sprint2.Task1.V10.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KopylskikhVM.Sprint2.Task1.V10.Lib;
+
+namespace Tyuiu.KopylskikhVM.Sprint2.Task1.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+
+            int a = 455;
+            int b = 335;
+            int c = 14;
+            int d = 17;
+
+            bool[] res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = { true, false, false, false, true, false };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KopylskikhVM.Sprint2.Task1.V10/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.KopylskikhVM.Sprint2.Task1.V10.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Копыльских В. М. | НТМ-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Операции сравнения и логические операции                          *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнила: Копыльских Виктория Максимовна | НТм-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу из операций сравнений и логических операций,         *");
+        Console.WriteLine("* которая вернет логическую последовательность:                           *");
+        Console.WriteLine("* (True, False, False, False, True, False)                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int a = 455;
+        int b = 335;
+        int c = 14;
+        int d = 17;
+
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("c = " + c);
+        Console.WriteLine("d = " + d);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        bool[] res = ds.GetLogicOperations(a, b, c, d);
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        Console.ReadLine();
+    }
+}
