7047f67b47a52576f0a1ea050776a0e18f3710c1
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-28 20:14:04 +0500

    Add project files.

---
+++ Tyuiu.KopylskikhVM.Sprint2.Task0.V8.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.KopylskikhVM.Sprint2.Task0.V8.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.KopylskikhVM.Sprint2.Task0.V8.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KopylskikhVM.Sprint2.Task0.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
