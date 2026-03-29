75cb80d12b0a0f1fd5817434c4230157c17c20ef
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-29 14:08:37 +0500

    Task3

--- Tyuiu.KopylskikhVM.Sprint2.Task3.V10.Test/DataServiceTest.cs
+++ Tyuiu.KopylskikhVM.Sprint2.Task3.V10.Test/DataServiceTest.cs
@@ -7,15 +7,47 @@ namespace Tyuiu.KopylskikhVM.Sprint2.Task3.V10.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void ValidCalculate()
+        public void ValidCondition1()
         {
             DataService ds = new DataService();
 
             double x = 2;
-
             double res = ds.Calculate(x);
 
             Assert.AreEqual(-70.250, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+
+            double x = 0;
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(-1.000, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+
+            double x = -2;
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(0.036, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+
+            double x = -15;
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(-164.933, res);
         }
     }
 }
