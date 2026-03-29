eae6752a9eaafdfa210bb2ae8a4ae676a13edde9
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-29 13:43:28 +0500

    Task3

---
+++ Tyuiu.KopylskikhVM.Sprint2.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KopylskikhVM.Sprint2.Task3.V10.Lib;
+
+namespace Tyuiu.KopylskikhVM.Sprint2.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            double x = 2;
+
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(-70.250, res);
+        }
+    }
+}
---
+++ Tyuiu.KopylskikhVM.Sprint2.Task3.V10/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.KopylskikhVM.Sprint2.Task3.V10.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Копыльских В. М. | НТМ-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Вложенные операторы if-else                                       *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнила: Копыльских Виктория Максимовна | НТм-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Вычислить значение функции Y по заданной формуле.                       *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*  y = {                                                                  *");
+        Console.WriteLine("*      x - ((x+15)/(x-4))^x,           если x > 0                         *");
+        Console.WriteLine("*      (x^2+cos(x^2)-3)/(x^2-sin(x^2)+2), если x = 0                      *");
+        Console.WriteLine("*      (5+1/x^2)^x,                   если -14 < x < 0                    *");
+        Console.WriteLine("*      x + 10x - (1/x),               если x < -14                        *");
+        Console.WriteLine("*  }                                                                      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("* Ответ округлить до 3 знаков после запятой.                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x;
+
+        Console.WriteLine("Введите значение x:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x));
+
+        Console.ReadLine();
+    }
+}
